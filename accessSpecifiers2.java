bcae4111cad9c36c903c5c795b2c479386c0ba8f
sneharajmane <sneharajmane96@gmail.com>
2022-02-24 12:38:36 +0530

    :adding

---
+++ accessSpecifiers2.java
@@ -0,0 +1,25 @@
+package com.phase.project;
+class priaccessspecifier 
+{ 
+   private void display() 
+    { 
+        System.out.println("You are using private access specifier"); 
+    } 
+} 
+
+public class accessSpecifiers2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//private
+				System.out.println("Private Access Specifier");
+				priaccessspecifier  obj = new priaccessspecifier(); 
+		        //trying to access private method of another class 
+		        //obj.display();
+
+			}
+		}
+
+	
+
+
---
+++ accessSpecifiers3.java
@@ -0,0 +1,10 @@
+package com.phase.project;
+
+public class accessSpecifiers3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
